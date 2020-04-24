bacf178bc577e4aabd052c76d1a9e79b487575bd
Jonatan Jimenez <jajimenezpa@poligran.edu.co>
2020-04-24 17:10:35 -0500

    jonatanj1293

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { TweetComponent } from './tweet/tweet.component';
-//import { ModelComponent } from './model/user.component';
 import { LoginComponent } from './login/login.component';
 import { TwittersComponent } from './twitters/twitters.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UserEditComponent } from './user-edit/user-edit.component';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent, pathMatch: 'full'},
   {path: 'register', component: RegisterComponent, pathMatch: 'full'},
   {path: 'profile/:id', component: ProfileComponent, pathMatch: 'full'},
-  {path: 'tweets', component: TweetersComponent, pathMatch: 'full'}
+  //{path: 'tweets', component: TweetersComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     TweetComponent,
-    //ModelComponent,
     LoginComponent,
     TwittersComponent,
     RegisterComponent,
--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,5 +1,11 @@
-
+import { Tweet} from './tweetModel';
 
 export class User {
-    constructor(public id: number, public username: string, public pass: string, public creation_date:Date ){}
+    constructor(public id: number, public username: string, public pass: string,
+                public tweet: Array<Tweet>) {
     }
+
+    static fromJson(json): User {
+      return new User(json.id, json.username, json.pass, json.tweet);
+    }
+}
